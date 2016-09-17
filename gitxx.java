1bbeefe41ccfc664c0737a13f28426663b321da0
Jesus Ivan <jim15mr@gmail.com>
2016-09-16 23:19:36 -0700

    Creacion de variables

---
+++ gitxx.java
@@ -0,0 +1,12 @@
+java.util.Scanner;
+public class gitxx{
+	public static void main(String []args){
+	
+		String nombre;
+		String apellidos;
+		int edad;
+		int nocontrol;
+		String fecha_nac;
+
+	}
+}
